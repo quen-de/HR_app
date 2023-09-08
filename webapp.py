1d266a96672d36aaf9ae75a9bff754a46c60e525
Quentin Debard <46495765+quen-de@users.noreply.github.com>
2023-09-08 09:16:56 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -51,7 +51,7 @@ if asked_tim:
                 # The azureml-model-deployment header will force the request to go to a specific deployment.
                 # Remove this header to have the request observe the endpoint traffic rules
                 # headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'people-faiss-ugly-uploadv4-1'}
-
+                headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'people-faiss-ugly-uploadv4-1' }
                 req = urllib.request.Request(url, body, headers)
 
                 try:
