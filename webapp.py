13cabf973f7a93552293ec8827f196a865a0bd52
Quentin Debard <46495765+quen-de@users.noreply.github.com>
2023-09-08 10:31:26 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -26,7 +26,16 @@ allowSelfSignedHttps(True) # this line is needed if you use self-signed certific
 st.set_page_config(page_title='Tim Sbott', page_icon="✅", layout="wide")
 
 st.title("Tim Sbott - Your (almost) perfect HR assistant")
-st.image('./res/tim_transparent.png')
+col1, col2 = st.columns(2)
+with col1:
+    st.image('./res/tim_transparent.png')
+with col2:
+    st.title('_Examples_')
+    st.text('How many holiday days can roll over to the next year?')
+    st.text('What should I do if a colleague harasses me?')
+    st.text('Where can I access my payslips?')
+    st.text('Can I cancel a holiday I previously booked?')
+    st.text('What is Fletcher\'s stance on diversity?')
 col1, col2 = st.columns(2)
 with col1:
     query = st.text_input('Question:')
@@ -50,15 +59,19 @@ if asked_tim:
 
                 # The azureml-model-deployment header will force the request to go to a specific deployment.
                 # Remove this header to have the request observe the endpoint traffic rules
-                # headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'people-faiss-ugly-uploadv4-1'}
                 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'people-faiss-ugly-uploadv4-1' }
+
                 req = urllib.request.Request(url, body, headers)
 
                 try:
                     response = urllib.request.urlopen(req)
 
                     result = response.read().decode("utf-8", 'ignore')[2:-2].replace('\\n', '\n')
-                except:
-                    st.write("Oops, Tim's currently out of the office...")
+                except urllib.error.HTTPError as error:
+                    print("The request failed with status code: " + str(error.code))
+
+                    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    print(error.info())
+                    print(error.read().decode("utf8", 'ignore'))
                     result = ''
             st.write(result)
